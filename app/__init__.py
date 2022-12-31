d9c935a24c7089a5d9cb7f45873d64761362ac9b
Milton Chang <mchangpi@gmail.com>
2022-12-31 22:00:53 +0800

    L34 Logging Users In

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from config import MiltonConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__) # __name__: the name of the current module
 
 app.config.from_object(MiltonConfig)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
