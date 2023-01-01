9ac2f3c7e001121e0e7eca443bc69e673a3b08d4
Milton Chang <mchangpi@gmail.com>
2023-01-01 19:27:13 +0800

    L37 Showing the logged in user in templates

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 from app import app
-from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import LoginForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'milton'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -18,7 +19,7 @@ def index():
             'body': 'What A beautiful day!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -31,7 +32,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netlog != '': #netlog: network location
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form = form)
 
 @app.route('/logout')
