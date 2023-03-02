a1c63123de79aab4812b9036c277a2dcefec583e
Milton Chang <mchangpi@gmail.com>
2023-03-02 13:58:05 +0800

    L60 Unit Testing the User Model

--- app/models.py
+++ app/models.py
@@ -4,14 +4,26 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    #declare 'author' property on the Post class
+    posts = db.relationship('Post', backref='author', lazy='dynamic') 
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id), #the left side entity (the follower user)
+        secondaryjoin=(followers.c.followed_id == id), #the right side entity (the followed user)
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -25,6 +37,23 @@ class User(UserMixin, db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user) -> bool:
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+    
+    def followed_posts(self) -> list:
+        following_posts = Post.query.join(followers, Post.user_id == followers.c.followed_id).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return following_posts.union(own).order_by(Post.timestamp.desc())
 
 @login.user_loader
 def load_user(id):
