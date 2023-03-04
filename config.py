a7321b4bccf6787177e1dbebf85eafc14d814fb4
Milton Chang <mchangpi@gmail.com>
2023-03-04 23:03:33 +0800

    L66 Pagination of Blog Posts

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class MiltonConfig(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['mchangpi@gmail.com']
+    POSTS_PER_PAGE = 3
